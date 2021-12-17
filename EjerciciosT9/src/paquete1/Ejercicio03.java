22ab2f62313d16a07cfff01f0072a9ea81775f97
JostinAlvarado552 <jealvarado2@utpl.edu.ec>
2021-12-17 11:46:54 -0500

    primer commit

--- EjerciciosT9/src/paquete1/Ejercicio03.java
+++ EjerciciosT9/src/paquete1/Ejercicio03.java
@@ -16,10 +16,30 @@ public class Ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        int elementosArribaMedia;
-        int elementosAbajoMedia;
-        
+        double[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
+        int elementosArribaMedia = 0;
+        int elementosAbajoMedia = 0;
+        double media = 0;
+        double suma = 0;
+        for (int i = 0; i < arreglo.length; i++) {
+
+            suma = suma + arreglo[i];
+
+        }
+
+        media = suma / arreglo.length;
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] < media) {
+                elementosAbajoMedia = elementosAbajoMedia + 1 ;
+            } else {
+                elementosArribaMedia = elementosArribaMedia + 1;
+            }          
+        }
+        System.out.printf("media:%.2f\nelementos arriba media:%d\n"
+                + "elementos abajo media:%d", 
+                media, 
+                elementosArribaMedia, 
+                elementosAbajoMedia);
     }
 
 }
